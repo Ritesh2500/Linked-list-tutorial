38eb788f0362427476b714a017224334b2e7a406
Ritesh2500 <riteshyadav3655@gmail.com>
2026-09-22 11:18:58 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    struct node* prev;
+    int data;
+    struct node* next;
+};
+struct node* addition( struct node *head, int data){
+    struct node* temp=malloc(sizeof(struct node));
+    temp->prev=NULL;
+    temp->data=data;
+    temp->next=NULL;
+    head=temp;
+    return head;
+}
+
+int main(){
+struct node* head=NULL;
+head=addition(head,45);
+    printf("%d ", head->data);
+
+    
+    return 0;
+}
